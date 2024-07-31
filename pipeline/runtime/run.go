bd90807ea966dce18284a60d9d0973b6377d9e59
Anurag Madnawat <anurag.madnawat@harness.io>
2024-07-31 09:22:05 -0700

    [feat]: [CI-12485]: Update lite-engine (#32)
    * [feat]: [CI-12485]: Update lite-engine
    
    * Update util.go
    
    * New lite engine

--- pipeline/runtime/run.go
+++ pipeline/runtime/run.go
@@ -64,7 +64,7 @@ func executeRunStep(ctx context.Context, engine *engine.Engine, r *api.StartStep
 
 	exited, err := engine.Run(ctx, step, out)
 	logrus.WithField("step_id", r.ID).WithField("stage_id", r.StageRuntimeID).Traceln("completed step run")
-	if rerr := report.ParseAndUploadTests(ctx, r.TestReport, r.WorkingDir, step.Name, log, time.Now(), tiConfig); rerr != nil {
+	if rerr := report.ParseAndUploadTests(ctx, r.TestReport, r.WorkingDir, step.Name, log, time.Now(), tiConfig, r.Envs); rerr != nil {
 		logrus.WithError(rerr).WithField("step", step.Name).Errorln("failed to upload report")
 	}
 
