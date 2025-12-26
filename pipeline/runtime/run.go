8873b6e902ca779cef84d628976c02cddbc8bb28
Ebtasam Faridy <ebtasam.faridy@harness.io>
2025-12-26 19:00:29 +0530

    fix: [CI-14462] adding change for supporting output variable

--- pipeline/runtime/run.go
+++ pipeline/runtime/run.go
@@ -167,7 +167,7 @@ func executeRunStep(ctx context.Context, engine *engine.Engine, r *api.StartStep
 			return exited, outputs, artifact, outputsV2, string(optimizationState), telemetry, finalErr
 
 		} else {
-			outputs, err := fetchOutputVariables(outputFile, out, false) // nolint:govet
+			outputs, err := fetchExportedVarsFromEnvFile(outputFile, out) // nolint:govet
 			if err != nil {
 				return exited, nil, nil, nil, string(optimizationState), telemetry, err
 			}
