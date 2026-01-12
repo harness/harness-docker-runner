fd29ecb345b431f9eb8d5e345e29459ebca2310a
Gargi <gargi@harness.io>
2026-01-12 17:42:26 +0530

    injectHcliPath for windows cont for runtest and runtestsV2

--- pipeline/runtime/runtestsV2.go
+++ pipeline/runtime/runtestsV2.go
@@ -82,6 +82,9 @@ func executeRunTestsV2Step(ctx context.Context, engine *engine.Engine, r *api.St
 	step.Envs["HARNESS_ANNOTATIONS_FILE"] = annotationsFile
 	// Set step ID (identifier from YAML) so hcli can populate it in annotations JSON
 	step.Envs["HARNESS_STEP_ID"] = step.Name
+	
+	// For Windows containers, inject hcli directory into PATH
+	injectHcliPathForWindowsContainer(step)
 
 	artifactFile := fmt.Sprintf("%s/%s-artifact", pipeline.GetSharedVolPath(), step.ID)
 	step.Envs["PLUGIN_ARTIFACT_FILE"] = artifactFile
